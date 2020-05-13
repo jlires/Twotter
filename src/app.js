2b059964836d0763d7b02c64526ddf8f4e103a39
Juanfcolopez <jflopez2@uc.cl>
2020-05-13 19:15:53 -0400

    Add Initial form for installation

--- src/app.js
+++ src/app.js
@@ -428,3 +428,44 @@ subCheckbox.addEventListener("change", async(event) => {
   }
 
 });
+
+const cancelinstallation = document.getElementById('Cancel');
+
+const closeInstallation = () =>{
+    const formInstall = document.getElementById('installform');
+    formInstall.style.display = 'none';
+};
+
+
+cancelinstallation.addEventListener('click', closeInstallation);
+
+window.onload = (e) => { 
+    // Declare init HTML elements
+    const buttonAdd = document.querySelector('#buttonAdd');
+
+    let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+    });
+
+    // Add event click function for Add button
+    buttonAdd.addEventListener('click', (e) => {
+        const formInstall = document.getElementById('installform');
+        formInstall.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+    });
+  }
